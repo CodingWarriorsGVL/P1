8e385c987725d4b2e1f22fdf1a457b6dd3a355bf
SACOLE2527 <SACOLE2527@4WLVG42.ad.uauth.net>
2019-03-22 12:53:06 -0400

    generator class progress

--- DUNG/src/navigation/generator.java
+++ DUNG/src/navigation/generator.java
@@ -1,17 +1,29 @@
+//generator.java | Author: ColeProtocol | Date: 2019.22.15 | Rev: 2019.22.15
+import java.util.Random;
 package navigation;
 
 import java.util.*;
 
-public abstract class generator {
-	public static genDungeon()
+public abstract class generator 
+{
+	Random geny = new Generator();
+   //for creation of a random dungeon arrangement
+	public static genDungeonRand()
 	{
-		
+		Dungeon dung = new Dungeon();
+		dung.getFloor(0).setRoom(new Room(wall, door, door, wall),  0, 0);
 	}
 	public static genFloor()
 	{
 		
 	}
-	
-	
+	public static genFeatures()
+	{
+		
+	}
+	public static genRoom()
+	{
+		
+	}
 
 }
